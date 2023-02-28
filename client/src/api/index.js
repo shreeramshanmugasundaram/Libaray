fccb8d312e6c92453a84a12f16adddc7af59cef8
shreeramshanmugasundaram <109068519+shreeramshanmugasundaram@users.noreply.github.com>
2023-02-28 18:22:45 +0530

    Delete client directory

--- client/src/api/index.js
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-const API = axios.create({ baseURL: "" });
-
-export const contact = (formData) => API.post("/contact", formData);
-export const email = (formData) => API.post("/email", formData);
-// shreeram
